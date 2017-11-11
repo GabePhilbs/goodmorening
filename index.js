a76c5528e12485d75982b3f22eb6ebde72e8a342
Gabe <gabriel.philbois@gmail.com>
2017-11-10 23:53:25 -0600

    can retrieve date

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+//make sure script is running
+
+console.log("goodmorening running");
+
+//get date
+var date = new Date();
+
+//set var for minutes and for hours
+var current_hour = date.getHours();
+var current_minutes = date.getMinutes();
+
+
+
+
+//check if date variables are being retrieved
+console.log(current_hour);
+console.log(current_minutes);
