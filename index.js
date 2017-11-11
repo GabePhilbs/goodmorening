ad6f2249daed34e8c94977c67d73f3f30e220209
Gabe <gabriel.philbois@gmail.com>
2017-11-11 02:41:02 -0600

    can execute function at certain hour

--- index.js
+++ index.js
@@ -4,16 +4,37 @@
 
 console.log("goodmorening running");
 
-//get date
-var date = new Date();
 
-//set var for minutes and for hours
-var current_hour = date.getHours();
-var current_minutes = date.getMinutes();
+//user defined variables
+
+var alarmHour = 2;
+var alarmminutes = 34;
+var alarmOn = true;
+
+
+
 
 
 
 
 //check if date variables are being retrieved
-console.log(current_hour);
-console.log(current_minutes);
+//console.log(currentHour);
+//console.log(currentMinutes);
+
+
+while (alarmOn == true) {
+
+	//get date
+	var date = new Date();
+
+	//set var for minutes and for hours
+	var currentHour = date.getHours();
+	var currentMinutes = date.getMinutes();
+
+
+
+	if(currentHour == alarmHour && currentMinutes == alarmminutes){
+		console.log("ring ring ring");
+		alarmOn = false;
+	}
+};
